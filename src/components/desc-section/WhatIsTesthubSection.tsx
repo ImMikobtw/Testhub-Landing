7592bb74a8b3d72abeb2b6156ec73e3ffad20ec3
ImMikobtw <mrsnakerpg@gmail.com>
2025-07-04 15:52:38 +0500

    Some fixes in desc. section

--- src/components/desc-section/WhatIsTesthubSection.tsx
+++ src/components/desc-section/WhatIsTesthubSection.tsx
@@ -42,7 +42,7 @@ const WhatIsTesthubSection: React.FC = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-4">
           {cards.map((card, index) => (
             <div
               key={index}
